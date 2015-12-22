22d2ac4eb249abbdecc8aa296bcde789d404d7a2
Paolo T. Inocencion <paoloinocencion@gmail.com>
2015-12-22 06:39:00 -0500

    final

--- app/src/main/java/net/androidbootcamp/sevenwondersofdcapp/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/sevenwondersofdcapp/MainActivity.java
@@ -1,5 +1,14 @@
 package net.androidbootcamp.sevenwondersofdcapp;
 
+/*
+ Created by: Paolo Inocencion
+ December 22, 2015
+ Sources:
+ All photos from google images, splashscreen was created using MSPaint.
+ Some lines of code are from "PersonalPhotoApp" assingment5.
+
+ */
+
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -35,31 +44,45 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        Toast.makeText(getBaseContext(), "UDC Jazz Archives ", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "UDC Jazz Archives " +
+                                "\n4200 Connecticut Ave NW, Washington, DC 20008. ",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 1:
-                        Toast.makeText(getBaseContext(), "Lincoln Memorial", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Lincoln Memorial " +
+                                "\n2 Lincoln Memorial Cir NW, Washington, DC 20037",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 2:
-                        Toast.makeText(getBaseContext(), "National Zoo", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "National Zoo " +
+                                "\n3001 Connecticut Ave NW, Washington, DC 20008",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 3:
-                        Toast.makeText(getBaseContext(), "US Capitol", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "US Capitol " +
+                                "\nEast Capitol St NE & First St SE, Washington, DC 20004",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 4:
-                        Toast.makeText(getBaseContext(), "Washington Monument", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Washington Monument " +
+                                "\n2 15th St NW, Washington, DC 20007\n",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 5:
-                        Toast.makeText(getBaseContext(), "The White House", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "The White House " +
+                                "\n1600 Pennsylvania Ave NW, Washington, DC 20500",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                     case 6:
-                        Toast.makeText(getBaseContext(), "World War 2 Memorial", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "World War 2 Memorial " +
+                                "\n1750 Independence Ave SW, Washington, DC 20006",
+                                Toast.LENGTH_SHORT).show();
                         pic.setImageResource(Wonders[position]);
                         break;
                 }
--- app/src/main/java/net/androidbootcamp/sevenwondersofdcapp/SplashActivity.java
+++ app/src/main/java/net/androidbootcamp/sevenwondersofdcapp/SplashActivity.java
@@ -1,5 +1,15 @@
 package net.androidbootcamp.sevenwondersofdcapp;
 
+/*
+ Created by: Paolo Inocencion
+ December 22, 2015
+ Sources:
+ All photos from google images, splashscreen was created using MSPaint.
+ Some lines of code are from "PersonalPhotoApp" assingment5.
+
+
+ */
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,14 +24,17 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Timer for how long splash screen shows.
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
+                // Starts MainActivity after timer expires.
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
         };
         Timer opening = new Timer();
-        opening.schedule(task, 500);
+        // 5500ms = 5.5seconds
+        opening.schedule(task, 5500);
     }
 }
